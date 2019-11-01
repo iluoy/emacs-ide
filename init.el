4c05f37e0f5eb2c7f19901abe4fd635c5b707895
yao <2593928094@qq.com>
2019-11-01 20:50:37 +0800

    new

--- init.el
+++ init.el
@@ -27,3 +27,17 @@
 
 ;; 加载init-org feature
 (require 'init-org)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (elpy use-package exec-path-from-shell ivy company molokai-theme monokai-theme spacemacs-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
